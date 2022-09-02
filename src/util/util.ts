9d894c3ee07cbcc3b0679712171b6b359dd82c85
Muhammad <m.afifi.cs@gmail.com>
2022-09-03 00:11:42 +0300

    Complete the new end point to process on Image URL

--- src/util/util.ts
+++ src/util/util.ts
@@ -9,9 +9,28 @@ import Jimp = require("jimp");
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+      Jimp.read(inputURL).then(photo => {
+          const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+          photo
+              .resize(256, 256) // resize
+              .quality(60) // set JPEG quality
+              .greyscale() // set greyscale
+              .write(__dirname + outpath, (img) => {
+                  resolve(__dirname + outpath);
+              });
+      }).catch(err => {
+          console.error(err);
+          reject("Could not read image.");
+      })
+  });
+}
+export async function filterImageFromURL2(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
+      console.log("I'm here")
       const photo = await Jimp.read(inputURL);
+      console.log(photo)
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
